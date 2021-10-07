b70d7e83c11584cd46da2de13916dcfbf4a46a7b
Osteh <74655634+Ostehura@users.noreply.github.com>
2021-10-07 19:23:24 +0300

    Add index.js with chart

---
+++ components/header.js
@@ -0,0 +1,20 @@
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link><Link href="/">Home</Link></Nav.Link>
+            <Nav.Link><Link href="/about-us">About project</Link></Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
