d4318f8c3d52dd959c8c63c8950ba4963977288c
Osteh <74655634+Ostehura@users.noreply.github.com>
2021-10-18 13:00:46 +0300

    add about-us.js

---
+++ pages/about-us.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default function AboutUsPage() {
+    return (<>
+        {/* <Nav activeKey="/about-us" variant="pills" >
+        <Nav.Item>
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/about-us">About us</Nav.Link>
+        </Nav.Item>
+      </Nav> */}
+    </>)
+}
