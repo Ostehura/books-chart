b617d706c936ae541ab3b15e4195aca92cf36eff
Osteh <74655634+Ostehura@users.noreply.github.com>
2021-10-18 16:52:33 +0300

    Update header

--- components/header.js
+++ components/header.js
@@ -5,13 +5,12 @@ export default function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        
+        <Navbar.Brand href="/">Book chart</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link href="/">Home</Link></Nav.Link>
-            <Nav.Link><Link href="/about-us">About project</Link></Nav.Link>
-
+            <Nav.Link > <Link href="/" >Home</Link></Nav.Link>
+            <Nav.Link > <Link href="/about-us">About project</Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
